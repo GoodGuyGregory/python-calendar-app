c04f883cdcfdb84dcab40a9cc647c30c85cecacc
GoodGuyGregory <greg.witt625@gmail.com>
2020-03-19 22:30:34 -0400

    adds notes and working project code

--- EventCreation/EventCreation.py
+++ EventCreation/EventCreation.py
@@ -1,91 +1,39 @@
-from __future__ import print_function
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
+# Allows for builing elements from google auth
+from apiclient.discovery import build
+# Flows are used to set up Oauthcredentials to the application
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+#  Saves Python Objects: in this case it will store our credentials
 
+import pickle
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+# Permissions to take from the user
+#  Found Here: https://developers.google.com/calendar/auth
+SCOPE = ['https://www.googleapis.com/auth/calendar']
 
-    service = build('calendar', 'v3', credentials=creds)
+# Create the Flow for Authentication:
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret.json", scopes=SCOPE)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
+credentials = flow.run_console()
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+# 4/xwHXGn8KomnppAscbzts9PWNlRCe
+# AUWnEh-_kRqVQfAJ5vtTWVPYVSc
 
+# Store the Credentials Created
+pickle.dump(credentials, open("token.pkl", "wb"))
 
-if __name__ == '__main__':
-    main()
+credentials = pickle.load(open("token.pkl", "rb"))
+service = build("calendar", "v3", credentials=credentials)
 
+#  Get All Calendars:
+result = service.calendarList().list().execute()
 
-# Refer to the Python quickstart on how to setup the environment:
-# https://developers.google.com/calendar/quickstart/python
-# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-# stored credentials.
+# Returns the First Calendar Items
+result['items'][0]
 
-event = {
-    'summary': 'Google I/O 2015',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-        'dateTime': '2015-05-28T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'end': {
-        'dateTime': '2015-05-28T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-    ],
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-            {'method': 'email', 'minutes': 24 * 60},
-            {'method': 'popup', 'minutes': 10},
-        ],
-    },
-}
+# Get all Events Listed
+calendar_id = result['items'][0]['id']
 
-event = service.events().insert(calendarId='primary', body=event).execute()
-print 'Event created: %s' % (event.get('htmlLink'))
+result = service.events().list(calendarId=calendar_id,
+                               timeZone="America/Kentucky").execute()
--- EventCreation/Notes.md
+++ EventCreation/Notes.md
@@ -2,4 +2,8 @@
 
 [Documentation](https://developers.google.com/calendar/create-events)
 
-[Video Tutorial](https://www.youtube.com/watch?v=tNo9IoZMelI&t=22s)
+[Git Gist Documentation](https://gist.github.com/nikhilkumarsingh/8a88be71243afe8d69390749d16c8322)
+
+[Video Tutorial](https://www.youtube.com/watch?v=j1mh0or2CX8)
+
+[Sponge Cloud Article on OAuthentication](https://www.cloudsponge.com/blog/google-oauth-reviews/)
